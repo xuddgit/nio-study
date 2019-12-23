216dc9685d425aa8e635a0c73fe74defe3e56e56
xudongdong <1107582053@qq.com>
2019-12-23 23:12:01 +0800

    版本初始化提交

---
+++ src/main/java/com/io/nio/SystemConfig.java
@@ -0,0 +1,24 @@
+/*
+ *company:jlc
+ *author:xudd
+ *date:2019/11/29:17:25
+ *desc:{}
+ **/
+
+
+package com.io.nio;
+
+/**
+ * @author:xudd
+ * @date:2019/11/29 -17:25
+ * @desc:
+ **/
+public class SystemConfig {
+
+    public static final Integer SOCKET_SERVER_PORT=8080;
+
+
+    public static final Integer INPUT_SIZE=100;
+
+    public static final Integer SEND_SIZE=100;
+}
